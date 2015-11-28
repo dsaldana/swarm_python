03e52c9dffe5cd83895ace07ff232bd8dc8b27c6
david <dajulian@gmail.com>
2015-11-28 05:16:51 -0500

    gazebo localization

--- controller.py
+++ controller.py
@@ -11,14 +11,14 @@ def update(robot_id):
     robot.set_linear_velocity(robot_id, 1, 0, 0)
     robot.set_angular_velocity(robot_id, 0, 0, .1)
 
-    print robot.gazebo_pose(robot_id)
+    # print robot.gazebo_pose(robot_id)
 
     # print robot.neighbors(robot_id)
     # print "pose:", robot.pose(robot_id)
     # print "search_area:", robot.search_area(robot_id)
     # print "camera",robot_id,":", robot.camera(robot_id)
 
-    # for neigh in robot.neighbors(robot_id):
-    #     # print "->", neigh, robot.send_to(robot_id, "hi ", neigh)
-    #     robot.send_to(robot_id, "hi ", neigh)
-    #     # print "-------"
+    for neigh in robot.neighbors(robot_id):
+        # print "->", neigh, robot.send_to(robot_id, "hi ", neigh)
+        robot.send_to(robot_id, "hi ", neigh)
+        # print "-------"
