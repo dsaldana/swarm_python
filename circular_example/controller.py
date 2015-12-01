4cf123b91e5311948d1883bf43593328223912bc
david <dajulian@gmail.com>
2015-12-01 17:22:36 -0500

    encirclement task with 36 robots

--- circular_example/controller.py
+++ circular_example/controller.py
@@ -4,22 +4,27 @@ import numpy as np
 import math
 
 # gain constants
-kp, kphi = .1, .05
+import sys
+
+kp, kphi = 1.5, .3
 
 # Desired radius
-R = .5
+R = 4.
 # Desired angular speed
-Omeg = 1.1
+Omeg = 0.5
 
 inbox = {}
 
-n = 2
+n = 36
 
 
 old_phi = np.zeros(n)
 
+
 def on_data_received(robot_id, src_add, dst_add, port, data):
     global inbox
+    # global n
+    # n = len(inbox)
     # print "new data"
     if robot_id not in inbox:
         inbox[robot_id] = {}
@@ -30,76 +35,77 @@ def on_data_received(robot_id, src_add, dst_add, port, data):
 
 
 def update(robot_id):
-    # Get pose
-    # print "1"
-    rx, ry, rz, rrx, rry, rrz = robot.gazebo_pose(robot_id)
+    try:
+        global old_phi
+        ########## Get pose #####################
+        # print robot_id, "1"
+        rx, ry, rz, rrx, rry, rrz = robot.gazebo_pose(robot_id)
 
-    rho = math.sqrt(rx ** 2 + ry ** 2)
-    phi = math.atan2(ry, rx)
+        rho = math.sqrt(rx ** 2 + ry ** 2)
+        phi = math.atan2(ry, rx)
 
-    # delta phi
-    dphi = math.atan2(math.sin(phi - old_phi[robot_id]), math.cos(phi - old_phi[robot_id]))
-    phi = old_phi[robot_id] + dphi
-    old_phi[robot_id] = phi
-    # Share location
-    # for neigh in robot.neighbors(robot_id):
-    #     if neigh == "boo":
-    #         continue
-    #
-    #     print "->", neigh, robot.send_to(robot_id, "hi ", neigh)
-    #     robot.send_to(robot_id, str(phi), neigh)
-    #     print "fff"
+        # delta phi
+        dphi = math.atan2(math.sin(phi - old_phi[robot_id]), math.cos(phi - old_phi[robot_id]))
+        phi = old_phi[robot_id] + dphi
+        old_phi[robot_id] = phi
 
-    # Next in ring
-    next = "192.168.1.%d" % ((robot_id + 1) % n + 1)
-    robot.send_to(robot_id, str(phi), next)
-    # last in ring
-    last = "192.168.1.%d" % ((robot_id - 1) % n + 1)
-    robot.send_to(robot_id, str(phi), last)
+        # Share location
+        # for neigh in robot.neighbors(robot_id):
+        #     if neigh == "boo":
+        #         continue
+        #
+        #     print "->", neigh, robot.send_to(robot_id, "hi ", neigh)
+        #     robot.send_to(robot_id, str(phi), neigh)
+        #     print "fff"
 
+        ############# Send location to the neighbors ##############
+        # Next in ring
+        next = "192.168.1.%d" % ((robot_id + 1) % n + 1)
+        robot.send_to(robot_id, str(phi), next)
+        # Before in ring
+        before = "192.168.1.%d" % ((robot_id - 1) % n + 1)
+        robot.send_to(robot_id, str(phi), before)
 
-    # print "inbox", inbox
-    # No inbox
-    if robot_id not in inbox:
-        print "Warning: no inbox for", robot_id
-        return
-    if last not in inbox[robot_id] or next not in inbox[robot_id]:
-        print "Warning: no data from neighbors for", robot_id
-        return
-
-
-
-    # Average Phi from neighbors
-    back1 = float(inbox[robot_id][last])
-    front1 = float(inbox[robot_id][next])
-    phi_av = (back1 + front1) / 2.
-
-    if robot_id == 0:
-        phi_av -= math.pi
-    if robot_id == n - 1:
-        phi_av += math.pi
-
-    # Controller
-    dot_rho = kp * (R - rho)
-    dot_phi = Omeg + kphi * (phi_av - phi)
-    #dot_phi = kphi * (math.pi * robot_id - phi)
-
-    dot_x = dot_rho * math.cos(phi) - rho * dot_phi * math.sin(phi)
-    dot_y = dot_rho * math.sin(phi) + rho * dot_phi * math.cos(phi)
-
-    if robot_id == 1:
-        print rho
-    # print robot_id, dot_phi
-    #print robot_id, next, last
-    # Tangent vector
+        # No inbox
+        if robot_id not in inbox:
+            print "Warning: no inbox for", robot_id
+            return
+        if before not in inbox[robot_id] or next not in inbox[robot_id]:
+            print "Warning: no data from neighbors for", robot_id
+            return
 
 
-    # Feedback linearization. Equation (12)
-    # d = .2
-    # v = wx * math.cos(rtheta) + wy * math.sin(rtheta)
-    # ang = - wx * math.sin(rtheta) / d + wy * math.cos(rtheta) / d
+        ############### Controller ##############
+        # Average Phi from neighbors
+        back1 = float(inbox[robot_id][before])
+        front1 = float(inbox[robot_id][next])
+        phi_av = (back1 + front1) / 2.
 
-    robot.set_linear_velocity(robot_id, dot_x, dot_y, 0)
-    # robot.set_angular_velocity(robot_id, 0, 0, ang)
+        if robot_id == 0:
+            phi_av -= math.pi
+        if robot_id == n - 1:
+            phi_av += math.pi
 
-    # print "vels: ", robot_id, v, ang, (rx, ry)
+        # Control input
+        dot_rho = kp * (R - rho)
+        dot_phi = Omeg + kphi * (phi_av - phi)
+
+        # Convert to cartesian coordinates
+        dot_x = dot_rho * math.cos(phi) - rho * dot_phi * math.sin(phi)
+        dot_y = dot_rho * math.sin(phi) + rho * dot_phi * math.cos(phi)
+
+        if robot_id == 3:
+            print rho
+
+
+        # Feedback linearization. Equation (12)
+        # d = .2
+        # v = wx * math.cos(rtheta) + wy * math.sin(rtheta)
+        # ang = - wx * math.sin(rtheta) / d + wy * math.cos(rtheta) / d
+
+        robot.set_linear_velocity(robot_id, dot_x, dot_y, 0)
+        # robot.set_angular_velocity(robot_id, 0, 0, ang)
+
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
